4e2b3fccb9acb3add82d3d8f09cd24338df2f6f2
Kalixer <al.caceres.ag72@gmail.com>
2022-02-12 16:00:05 -0300

    Mute/Unmute button added... This was a challenge

--- assets/index.js
+++ assets/index.js
@@ -2,11 +2,13 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video');
-const button = document.querySelector('button');
+const button_play = document.querySelector('button');
+const button_mute = document.getElementById("b_mute")
 
 
 const player = new MediaPlayer({ el: video, plugins: [
   new AutoPlay
 ] });
 
-button.onclick = () => { player.toggleplay() }
+button_play.onclick = () => { player.toggleplay() }
+button_mute.onclick = () => { player.unmute() }
