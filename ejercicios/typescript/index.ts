1f1cf8acee507d3db9ec9d0cc7caba9875570a90
Kalixer <al.caceres.ag72@gmail.com>
2022-03-04 18:00:23 -0300

    Basic types on TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,8 +1,45 @@
-console.log("Hello, TypeScript");
+// Boolean
+let muted: boolean = true;
+muted = false;
+// muted = "callado"
+// no funcionara, TypeScrip te asiste al decir que es de tipo booleano
 
-function add(a: number, b: number) {
-    return a + b;
+// Number
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = 'Alejandro';
+let saludo = `Mi nombre es ${nombre}`
+
+// Arrays
+let people: string[] | number[] = [];
+people = ["Isabel", "Nicole", "Raul",];
+// people.push("9000")
+
+let peopleAndNumbers: Array< string | number> = []
+
+// Enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
 }
 
-const sum = add(2, 3)
-console.log(sum)
+let colorFavorito: Color = Color.Rojo;
+console.log(`Mi color vaforito es el ${colorFavorito}`)
+
+// Any
+let comodin: any = "joker";
+comodin = { type: "Wildcard"}
+
+// Object
+let person: object = { name:"Luis" };
+
+// Tuple
+let x: [string, number] = ["hello", 10]
+let y: [string, number] = ["hi", 20]
+console.log(x)
+console.log(y)
