a3f1d6e91ca3608155f95c2e48edcc36aaec2759
Kalixer <al.caceres.ag72@gmail.com>
2022-03-05 16:20:31 -0300

    functiones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -54,3 +54,26 @@ function lista() {
 }
 lista()
 */
+
+// Funciones
+function add(a:number, b:number): number {
+    return a + b;
+}
+
+const sum = add(4, 6)
+
+function createAdder(a:number): (number) => number {
+    return function(b:number) {
+        return b + a;
+    }
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6)
+
+function fullName(fisrtName: string, lastName: string = 'Smith'): string {
+    return `${fisrtName} ${lastName}`
+}
+
+const alejandro = fullName('Alejandro')
+console.log(alejandro)
